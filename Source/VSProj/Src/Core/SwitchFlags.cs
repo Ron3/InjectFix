9070503f72b6f716e618e3b0014e2c7f5ac93f81
Ron3 <luodm03@qq.com>
2021-04-29 13:55:54 +0800

    -change: 让其支持版本信息.

--- Source/VSProj/Src/Core/SwitchFlags.cs
+++ Source/VSProj/Src/Core/SwitchFlags.cs
@@ -6,6 +6,10 @@
  */
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace IFix
 {
@@ -13,12 +17,57 @@ namespace IFix
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field)]
     public class InterpretAttribute : Attribute
     {
+        // public int ver = 0;
+        public int[] verArray;
+        public InterpretAttribute()
+        {
+            this.verArray = new int[1];
+            this.verArray[0] = 0;
+        }
+
+        public InterpretAttribute(params int[] v)
+        {
+            this.verArray = v;
+        }
+        
+        public int maxVer
+        {
+            get{
+                return this.verArray.Max();
+            }
+        }
     }
+
 
     //直接在要做成补丁的方法上打标签
     [AttributeUsage(AttributeTargets.Method)]
     public class PatchAttribute : Attribute
     {
+        // public int ver = 0;
+        // public List<int> verList;
+        public int[] verArray;
+        public PatchAttribute()
+        {
+            this.verArray = new int[1];
+            this.verArray[0] = 0;
+        }
+
+        // public PatchAttribute(int v)
+        // {
+            
+        // }
+
+        public PatchAttribute(params int[] v)
+        {
+            this.verArray = v;
+        }
+
+        public int maxVer
+        {
+            get{
+                return this.verArray.Max();
+            }
+        }
     }
 
     //可以手动指定要生成delegate（主要用于闭包）、interface（比如迭代器语法糖）的桥接
